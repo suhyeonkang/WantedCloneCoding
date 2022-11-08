dd47b0cd61326d36dee743c662649ac4c64e1b72
suhyeonkang <shkang2511@gmail.com>
2022-11-08 19:24:41 +0900

    modified slider's css(contents overflow)

--- src/mainSlickSlide.js
+++ src/mainSlickSlide.js
@@ -40,7 +40,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>마키나락스에 세로들어와!</h2>
                         <h3>적극 채용 중인 포지션을 영상으로 확인하세요!</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -50,7 +50,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>롯데헬스케어에 세로들어와!</h2>
                         <h3>적극 채용 중인 포지션을 영상으로 확인하세요!</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -60,7 +60,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>[FREE] GDSC Job Fair Live</h2>
                         <h3>국내 최고 스타트업 채용소식을 볼 수 있는 기회!</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -70,7 +70,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>Statup Lead Group Coaching</h2>
                         <h3>팀장을 위한 그룹코칭</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -78,9 +78,9 @@ function MainSlickSlide () {
                 <div id="main_1_slide">
                     <img src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fbanners%2F1843%2F6e4ca95f.jpg&w=1060&q=100" alt="main_slide_img5"></img>
                     <div>
-                        <h2>원티드긱스 '{'<'}'개발자의 달'{'>'}'</h2>
+                        <h2>원티드긱스 {'<'}개발자의 달{'>'}</h2>
                         <h3>프리랜서 개발자를 위한 이벤트</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -90,7 +90,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>기업이 원하는 프론트 개발자 되기</h2>
                         <h3>프리온보딩 프론트엔드 코스</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -100,7 +100,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>우리 회사를 소개합니다.</h2>
                         <h3>'회사'에 대한 정보, 원티드가 찾아드릴게요!</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -110,7 +110,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>10월 원티드살롱</h2>
                         <h3>HR담당자를 위한 네트워킹</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -120,7 +120,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>리텐션 높이는 '아하 모먼트'를 찾아서</h2>
                         <h3>성공을 리드하는 번개장터 마케터</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
@@ -130,7 +130,7 @@ function MainSlickSlide () {
                     <div>
                         <h2>단 4주! 백엔드 신입딱지 떼기</h2>
                         <h3>프리온보딩 백엔드 코스</h3><hr/>
-                        <button>바로가기 > </button>
+                        <button>바로가기 {'>'} </button>
                        
                     </div>
                 </div>
