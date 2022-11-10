e562c95a66b8c5c60409ae6e6401758d49563ddb
suhyeonkang <shkang2511@gmail.com>
2022-11-10 16:55:38 +0900

    modified modal`s state used redux

---
+++ src/modules/modalStore.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {createStore} from "redux";
+import { Provider, useSelector, useDispatch } from "react";
+
+
+// 액션 타입 정의
+
+const LoginOpen = 'modalStore/LoginOpen';
+const JoinOpen = 'modalStore/JoinOpen';
+const ModalClose = 'modalStore/ModalClose';
+const SearchOpen = 'modalStore/SearchOpen';
+const SearchClose = 'modalStore/SearchClose';
+
+// 액션 생성 함수 
+
+export const loginOpen = () => ({type: LoginOpen});
+export const joinOpen = () => ({type:JoinOpen});
+export const modalClose = () => ({type:ModalClose});
+export const searchOpen = () => ({type:SearchOpen});
+export const searchClose = () => ({type:SearchClose});
+
+
+// 초기상태 및 리듀서 함수
+
+const initialStates = {
+    modalOpen: 0,
+    searchFilter: false,
+};
+
+const reducer = (state = initialStates, action) => {
+    switch(action.type) {
+        case LoginOpen :
+            return {...state, modalOpen: 1}
+        case JoinOpen :
+            return {...state, modalOpen: 2}
+        case ModalClose :
+            return {...state, modalOpen: 0}
+        case SearchOpen :
+            return {...state, searchFilter: true}
+        case SearchClose :
+            return {...state, searchFilter: false}
+        default :
+            return state;                   
+    }
+}
+
+
+export default reducer;
+
+
+
+
